588217eea3b94d80b0fc3cc8831b11a4b6586561
dasha-shchep <ds17094@bristol.ac.uk>
2019-07-16 16:45:48 +0100

    Updated functions

--- kabsch_optimiser.jl
+++ kabsch_optimiser.jl
@@ -2,25 +2,37 @@
 # by minimising the RMSD between them.
 # Takes 2 .xyz files as input
 # Program executed as
-# 'julia kabsch_optimiser.jl geomA.xyz geomB.xyz'
+# 'julia kabsch_optimiser.jl geomP.xyz geomQ.xyz'
 # Darya Shchepanovska
 # 15th July 2019
 
 using DataFrames
 using CSV
 
-function read_xyz(input_xyz)
-    return CSV.read(input_xyz,ignoreemptylines=true)
+using LinearAlgebra
+using Statistics
+
+function xyz2matrix(input_xyz)
+    df = CSV.File(input_xyz,datarow=3,header=false,ignorerepeated=true,delim=' ') |> DataFrame
+    matrix = convert(Matrix,df[:,2:4])
+    return matrix
 end
 
 function translate_to_centroid(coordinate_matrix)
-    return coordinate_matrix
+    centriod = [mean(coordinate_matrix[:,1]),mean(coordinate_matrix[:,2]),mean(coordinate_matrix[:,3])]
+    return centriod
 end
 
 function cross_covariance_matrix(Pmatrix,Qmatrix)
-    return Pmatrix+Qmatrix
+    CCmatrix = transpose(Pmatrix) * Qmatrix
+    return CCmatrix
 end
 
 function optimal_rotation_matrix(CCmatrix)
-    return CCmatrix
+    return ORmatrix
 end
+
+Pgeom = xyz2matrix("test_geometries/hpaldA.xyz")
+Qgeom = xyz2matrix("test_geometries/hpaldB.xyz")
+
+println(translate_to_centroid(Qgeom))
