542d2b67032a8b17afa67f05dc7eba1214f64aba
dasha-shchep <ds17094@bristol.ac.uk>
2019-07-25 17:02:42 +0100

    Include RMSD calculation

--- kabsch_optimiser.jl
+++ kabsch_optimiser.jl
@@ -13,12 +13,14 @@ using LinearAlgebra
 using Statistics
 
 function xyz2matrix(input_xyz)
-    df = CSV.File(input_xyz,datarow=3,header=false,ignorerepeated=true,delim=' ') |> DataFrame
-    matrix = convert(Matrix,df[:,2:4])
+    # Imports .xyz file format as data frame, and converts it to an N x 3 Matrix
+    df=CSV.File(input_xyz,datarow=3,header=false,ignorerepeated=true,delim=' ')|>DataFrame
+    matrix=convert(Matrix,df[:,2:4])
     return matrix
 end
 
 function translate_to_centroid(coord_matrix)
+    # Normalises the molecular coordinates by centering them.
     center = [mean(coord_matrix[:,1]);mean(coord_matrix[:,2]);mean(coord_matrix[:,3])]
     centroid = transpose(center)
     translated_geom = broadcast(-,coord_matrix,centroid)
@@ -26,11 +28,13 @@ function translate_to_centroid(coord_matrix)
 end
 
 function cross_covariance_matrix(Pmatrix,Qmatrix)
+    # Cross covariance matrix gives measure of variability between two matrices
     CCmatrix = transpose(Pmatrix) * Qmatrix
     return CCmatrix
 end
 
 function optimal_rotation_matrix(CCmatrix)
+    # Returns 3x3 matrix that can be applied to P to get Q
     ORmatrix = sqrt(transpose(CCmatrix)*CCmatrix)*inv(CCmatrix)
     return ORmatrix
 end
@@ -42,7 +46,17 @@ normalisedP = (translate_to_centroid(Pgeom))
 normalisedQ = (translate_to_centroid(Qgeom))
 
 xcov = cross_covariance_matrix(normalisedP,normalisedQ)
-
 orot = optimal_rotation_matrix(xcov)
 
-display(orot)
+num_atoms = size(normalisedP)[1]
+rotated = zeros(Float64,num_atoms,3)
+
+for i=1:num_atoms
+    rotated[i,:] = orot*normalisedP[i,:]
+end
+
+# display(rotated)
+
+RMSD_value = norm(rotated-normalisedQ)
+
+println("RMSD between these two structures is ", RMSD_value)
